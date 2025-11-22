2865b5ab932228ce111e0e89ad75e3b9dcea6b51
leoSallos <sallosl@oregonstate.edu>
2025-11-22 15:23:11 -0800

    created skeleton of server

---
+++ server.js
@@ -0,0 +1,58 @@
+var fs = require("fs").promises
+var express = require("express")
+var app = express()
+app.use(express.static(__dirname));
+app.use(express.json())
+const port = 8007
+
+//
+// Middleware
+//
+
+app.use(function (req, res, next) {
+    // allow cross-origin access control
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+
+    next();
+});
+
+// 
+// Route Service
+//
+
+// server ping
+app.get("/", function(req, res) {
+    console.log("Ping recieved")
+    res.status(204);
+});
+
+// get user online status
+app.get("/list", function(req, res){
+});
+
+// ping online
+app.get("/online/:userID", function(req, res){
+});
+
+// post new user to database
+app.post("/new", function(req, res){
+});
+
+// start server listening
+app.listen(port, function(err){
+    if (err){
+        throw err;
+    } else {
+        console.log("Server listening on port " + port);
+    }
+});
+
+// write update to file
+async function writeUserList(){
+}
+
+// check last pings
+var userList = [];
+// TODO: Add 1min timer
